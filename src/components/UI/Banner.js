b31bf626a8399ecb8bbf7b09e12bd16d31427027
Zakia Aktar <zakiaaktar@Zakias-MacBook-Pro.local>
2023-10-10 10:56:02 +0200

    add banner section

--- src/components/UI/Banner.js
+++ src/components/UI/Banner.js
@@ -1,175 +1,127 @@
 import { Col, Row, Carousel } from "antd";
 import {
-  ArrowRightOutlined,
-  CalendarOutlined,
-  CommentOutlined,
-  ProfileOutlined,
+    ArrowRightOutlined,
 } from "@ant-design/icons";
 import Image from "next/image";
-import DrawingImage from "@/assets/images/banner-images/drawing_image.jpg";
-import EagleImage from "@/assets/images/banner-images/eagle_image.jpg";
+import Banner1Image from "@/assets/images/banner-images/banner1.jpg";
+import Banner3Image from "@/assets/images/banner-images/banner3.jpg";
 
 const contentStyle = {
-  height: "425px",
-  color: "#000",
+    height: "425px",
+    color: "#000",
 };
 
 const Banner = () => (
-  <Carousel effect="fade" autoplay style={{ margin: "20px 0px" }}>
-    {/* slider-1 */}
-    <div>
-      <Row>
-        <Col
-          lg={{
-            span: 8,
-          }}
-        >
-          <h1 style={{ fontSize: "50px" }}>
-            LET&apos;S BUILD
-            <br />
-            YOUR CAREER
-          </h1>
-          <div
-            className="line"
-            style={{
-              height: "5px",
-              margin: "20px 0",
-              background: "#000",
-              width: "95%",
-            }}
-          ></div>
+    <Carousel effect="fade" autoplay style={{ margin: "20px 0px" }}>
+        {/* slider-1 */}
+        <div>
+            <Row>
+                <Col
+                    lg={{
+                        span: 8,
+                    }}
+                >
+                    <h1 style={{ fontSize: "50px", color: "#E25E3E" }}>
+                        Get Your
+                        <br />
+                        Desired Product
+                    </h1>
+                    <div
+                        className="line"
+                        style={{
+                            height: "5px",
+                            margin: "20px 0",
+                            background: "#E25E3E",
+                            width: "95%",
+                        }}
+                    ></div>
+                    <p style={{ fontSize: "20px" }}>
+                    Your journey to the perfect custom PC starts here. Simply use our PC builder tool to select your components, and watch as your custom PC takes shape.
+                    </p>
+                    <p
+                        style={{
+                            fontSize: "20px",
+                            margin: "20px 0px",
+                            backgroundColor: "#E25E3E",
+                            color: "white",
+                            width: "168px",
+                            padding: "8px 7px ",
+                            fontWeight: "300",
+                            letterSpacing: "3px",
+                        }}
+                    >
+                        More Info <ArrowRightOutlined />
+                    </p>
+                </Col>
 
-          {/* <p
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              width: "90%",
-              color: "gray",
-              margin: "10px 0px",
-            }}
-          >
-            <span>
-              <CalendarOutlined /> FEBRUARY 28, 2023
-            </span>
-            <span>
-              <CommentOutlined /> NO COMMENTS
-            </span>
-            <span>
-              <ProfileOutlined /> HOBBY
-            </span>
-          </p> */}
+                <Col
+                    lg={{
+                        span: 16,
+                    }}
+                    style={contentStyle}
+                >
+                    <Image src={Banner1Image} fill alt="Banner1Image" />
+                </Col>
+            </Row>
+        </div>
+        {/* slider-2 */}
+        <div>
+            <Row>
+                <Col
+                    lg={{
+                        span: 8,
+                    }}
+                >
+                    <h1 style={{ fontSize: "50px", color: "#E25E3E" }}>
+                        Click Your
+                        <br />
+                        Desired Product
+                    </h1>
+                    <div
+                        className="line"
+                        style={{
+                            height: "5px",
+                            margin: "20px 0",
+                            backgroundColor: "#E25E3E",
+                            width: "95%",
+                        }}
+                    ></div>
 
-          <p style={{ fontSize: "20px" }}>
-            Beauteous before up across felt sheepishly and more mournfully the
-            wow so more flustered and one up pushed salamander collective
-            blinked that iguanodon bid much some since hey far goodness jaguar
-            whil...
-          </p>
-          <p
-            style={{
-              fontSize: "20px",
-              margin: "20px 0px",
-              backgroundColor: "black",
-              color: "white",
-              width: "168px",
-              padding: "2px 5px ",
-              fontWeight: "300",
-              letterSpacing: "3px",
-            }}
-          >
-            Keep Reading <ArrowRightOutlined />
-          </p>
-        </Col>
 
-        <Col
-          lg={{
-            span: 16,
-          }}
-          style={contentStyle}
-        >
-          <Image src={DrawingImage} fill alt="drawing_image" />
-        </Col>
-      </Row>
-    </div>
-    {/* slider-2 */}
-    <div>
-      <Row>
-        <Col
-          lg={{
-            span: 8,
-          }}
-        >
-          <h1 style={{ fontSize: "50px" }}>
-            EAGLE, YOU ARE
-            <br />
-            NOT ALONE
-          </h1>
-          <div
-            className="line"
-            style={{
-              height: "5px",
-              margin: "20px 0",
-              background: "#000",
-              width: "95%",
-            }}
-          ></div>
+                    <p style={{ fontSize: "20px" }}>
+                    Your journey to the perfect custom PC starts here. Simply use our PC builder tool to select your components, and watch as your custom PC takes shape.
+                    </p>
+                    <p
+                        style={{
+                            fontSize: "15px",
+                            margin: "20px 0px",
+                            backgroundColor: "#E25E3E",
+                            color: "white",
+                            width: "168px",
+                            padding: "8px 7px ",
+                            fontWeight: "300",
+                            letterSpacing: "3px",
+                        }}
+                    >
+                        More Info <ArrowRightOutlined />
+                    </p>
+                </Col>
 
-          <p
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              width: "90%",
-              color: "gray",
-              margin: "10px 0px",
-            }}
-          >
-            <span>
-              <CalendarOutlined /> MARCH 30, 2023
-            </span>
-            <span>
-              <CommentOutlined /> 5 COMMENTS
-            </span>
-            <span>
-              <ProfileOutlined /> NATURE
-            </span>
-          </p>
-
-          <p style={{ fontSize: "20px" }}>
-            A spread opened patient and compulsively one placed seagull goodness
-            python owing snapped yikes equitable when much the much Lorem ipsum
-            dolor sit, amet consectetur adipisicing elit. Eligendi, tenetur!...
-          </p>
-          <p
-            style={{
-              fontSize: "20px",
-              margin: "20px 0px",
-              backgroundColor: "black",
-              color: "white",
-              width: "168px",
-              padding: "2px 5px ",
-              fontWeight: "300",
-              letterSpacing: "3px",
-            }}
-          >
-            Keep Reading <ArrowRightOutlined />
-          </p>
-        </Col>
-
-        <Col
-          lg={{
-            span: 16,
-          }}
-          style={contentStyle}
-        >
-          <Image
-            src={EagleImage}
-            fill
-            alt="eagle_image"
-            style={{ grayScale: "-1" }}
-          />
-        </Col>
-      </Row>
-    </div>
-  </Carousel>
+                <Col
+                    lg={{
+                        span: 16,
+                    }}
+                    style={contentStyle}
+                >
+                    <Image
+                        src={Banner3Image}
+                        fill
+                        alt="Banner3Image"
+                        style={{ grayScale: "-1" }}
+                    />
+                </Col>
+            </Row>
+        </div>
+    </Carousel>
 );
 export default Banner;
